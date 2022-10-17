10a5f91e68174260cfa98a344b614371732308ec
rania sellami <rania.sellmi@esprit.tn>
2022-10-17 11:36:05 +0200

    update mock data

--- app/src/main/java/com/example/basketb/teams/MockData.kt
+++ app/src/main/java/com/example/basketb/teams/MockData.kt
@@ -7,6 +7,9 @@ val TeamsList = listOf(
         ( id:3, city:"bvjk",division:"3",name:"nano",conference:"hellao",)
         ( id:4, city:"ldfknos",division:"4",name:"Honey",conference:"allah",)
         ( id:5, city:"ofelp",division:"5",name:"ranouch",conference:"kd",)
+        ( id:5, city:"ofelp",division:"5",name:"ranouch",conference:"kd",)
+        ( id:5, city:"ofelp",division:"5",name:"ranouch",conference:"kd",)
+        ( id:5, city:"ofelp",division:"5",name:"ranouch",conference:"kd",)
 )
 
 
